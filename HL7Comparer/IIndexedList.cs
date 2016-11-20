8ebe565fb3f40e2898d6fd9ed748a5af14e12b89
Julien LEBOT <julien.ar.lebot@gmail.com>
2016-11-20 01:31:53 +0100

    Added missing file

---
+++ HL7Comparer/IIndexedList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace HL7Comparer
+{
+    /// <summary>
+    /// Represents a collection of objects that can be individually accessed by index.
+    /// The items are created on demand when they are accessed.
+    /// </summary>
+    /// <typeparam name="TIndex">The type of indices in the list.</typeparam>
+    /// <typeparam name="TValue">The type of elements in the list.</typeparam>
+    public interface IIndexedList<in TIndex, out TValue> : IEnumerable<TValue>, IEnumerable
+        where TValue : class
+    {
+        TValue this[TIndex index] { get; }
+    }
+}
